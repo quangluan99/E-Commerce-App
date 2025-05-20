5e647dec142e4bd32cd6f14d3ef1ddd559338fc2
quangluan99 <bqluan1902@gmail.com>
2025-05-21 01:34:30 +0700

    Edit layout

--- lib/view/role_based_login/user/screens/user_app_home_screen.dart
+++ lib/view/role_based_login/user/screens/user_app_home_screen.dart
@@ -29,161 +29,189 @@ class _UserAppHomeScreenState extends State<UserAppHomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
+          child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Column(
                 children: [
-                  Image.asset(
-                    'assets/logo.png',
-                    height: 36.0,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        'assets/logo.png',
+                        height: 36.0,
+                      ),
+                      const CartOderCount(),
+                    ],
                   ),
-                  const CartOderCount(),
+                  const SizedBox(
+                    height: 18.0,
+                  ),
+                  const MyBanner(),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const MyBanner(),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 18.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          Expanded(
+            flex: 5,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
                 children: [
-                  Text(
-                    'Shop By Category',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'See All',
-                    style: TextStyle(fontSize: 15.0, color: Colors.black45),
-                  )
-                ],
-              ),
-            ),
-            //for category
-            StreamBuilder(
-              stream: categoriesItems.snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-                if (streamSnapshot.hasData) {
-                  return SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 18.0),
                     child: Row(
-                      children: List.generate(
-                        streamSnapshot.data!.docs.length,
-                        (index) {
-                          return InkWell(
-                            onTap: () {
-                              // final filteritems = fashionEcomerceApp
-                              //     .where(
-                              //       (item) =>
-                              //           item.category.toLowerCase() ==
-                              //           category[index].name.toLowerCase(),
-                              //     )
-                              //     .toList();
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) {
-                                  return CategoryItems(
-                                    category: streamSnapshot.data!.docs[index]
-                                        ['name'],
-                                    selectedCategory: streamSnapshot
-                                        .data!.docs[index]['name'],
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Shop By Category',
+                          style: TextStyle(
+                              fontSize: 15.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black45,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  //for category
+                  StreamBuilder(
+                    stream: categoriesItems.snapshots(),
+                    builder:
+                        (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                      if (streamSnapshot.hasData) {
+                        return SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: List.generate(
+                              streamSnapshot.data!.docs.length,
+                              (index) {
+                                return InkWell(
+                                  onTap: () {
+                                    // final filteritems = fashionEcomerceApp
+                                    //     .where(
+                                    //       (item) =>
+                                    //           item.category.toLowerCase() ==
+                                    //           category[index].name.toLowerCase(),
+                                    //     )
+                                    //     .toList();
+                                    Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return CategoryItems(
+                                          category: streamSnapshot
+                                              .data!.docs[index]['name'],
+                                          selectedCategory: streamSnapshot
+                                              .data!.docs[index]['name'],
+                                        );
+                                      },
+                                    ));
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 15.0),
+                                        child: CircleAvatar(
+                                          radius: 28.0,
+                                          backgroundColor: fbackgroundColor1,
+                                          backgroundImage:
+                                              AssetImage(category[index].image),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      Text(category[index].name),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        );
+                      }
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    },
+                  ),
+
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 18.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Curated For you',
+                          style: TextStyle(
+                              fontSize: 15.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'See All',
+                          style:
+                              TextStyle(fontSize: 15.0, color: Colors.black45),
+                        )
+                      ],
+                    ),
+                  ),
+                  //for curated items
+                  StreamBuilder(
+                      stream: items.snapshots(),
+                      builder:
+                          (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasData) {
+                          return SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: List.generate(
+                                snapshot.data!.docs.length,
+                                (index) {
+                                  final eCommerceItems =
+                                      snapshot.data!.docs[index];
+                                  return Padding(
+                                    padding: index == 0
+                                        ? const EdgeInsets.symmetric(
+                                            horizontal: 18.0)
+                                        : const EdgeInsets.only(right: 18.0),
+                                    child: InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ItemsDetaiScreen(
+                                                productItems: eCommerceItems,
+                                              ),
+                                            ));
+                                      },
+                                      child: CuratedItems(
+                                          eCommerceItems: eCommerceItems,
+                                          size: size),
+                                    ),
                                   );
                                 },
-                              ));
-                            },
-                            child: Column(
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 15.0),
-                                  child: CircleAvatar(
-                                    radius: 28.0,
-                                    backgroundColor: fbackgroundColor1,
-                                    backgroundImage:
-                                        AssetImage(category[index].image),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Text(category[index].name),
-                              ],
+                              ),
                             ),
                           );
-                        },
-                      ),
-                    ),
-                  );
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            ),
-
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 18.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Curated For you',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'See All',
-                    style: TextStyle(fontSize: 15.0, color: Colors.black45),
-                  )
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      })
                 ],
               ),
             ),
-            //for curated items
-            StreamBuilder(
-                stream: items.snapshots(),
-                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasData) {
-                    return SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: List.generate(
-                          snapshot.data!.docs.length,
-                          (index) {
-                            final eCommerceItems = snapshot.data!.docs[index];
-                            return Padding(
-                              padding: index == 0
-                                  ? const EdgeInsets.symmetric(horizontal: 18.0)
-                                  : const EdgeInsets.only(right: 18.0),
-                              child: InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => ItemsDetaiScreen(
-                                          productItems: eCommerceItems,
-                                        ),
-                                      ));
-                                },
-                                child: CuratedItems(
-                                    eCommerceItems: eCommerceItems, size: size),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    );
-                  }
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                })
-          ],
-        ),
+          )
+        ],
       )),
     );
   }
